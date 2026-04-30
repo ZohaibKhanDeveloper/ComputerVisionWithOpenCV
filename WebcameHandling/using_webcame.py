66bb2ac7077de71607f82ece15b4c6c99565e0e5
Zohaib khan <zk510873@gmail.com>
2026-04-30 08:00:51 -0700

    Webcame Handling using open cv

---
+++ WebcameHandling/using_webcame.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Could not feed frame...")
+        break
+
+    cv2.imshow("Webcame Feeding...",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Quitting...")
+        break
+
+cap.release()
+cv2.destroyAllWindows()    
